f0d1db4688eefb864d3518cfdeb8716a20475d65
mushegh <mushegh@stdevmail.com>
2018-11-19 16:08:05 +0400

    change allowed hosts

--- django_bootstrap/settings.py
+++ django_bootstrap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '##2=gj#sunq=^5p^k3^3rrvjz=ee+zehz0*03(13!@g&ok!&!@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
